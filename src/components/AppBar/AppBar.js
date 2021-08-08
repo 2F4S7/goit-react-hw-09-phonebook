528e8db89e696a9c9afe21f23d5e079ef6ee3ca2
unknown <nightsta1kerhf@gmail.com>
2021-08-08 17:07:30 +0300

    v.9.2.0

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,45 +1,42 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { authSelectors } from '../../redux/auth';
-import { AppBar, makeStyles } from '@material-ui/core';
-import Navigation from '../Navigation';
-import AuthNav from '../AuthNav';
-import UserMenu from '../UserMenu';
-import { Container } from '@material-ui/core';
+import React from "react";
+import { useSelector } from "react-redux";
+import { authSelectors } from "../../redux/auth";
+import { AppBar, makeStyles } from "@material-ui/core";
+import Navigation from "../Navigation";
+import AuthNav from "../AuthNav";
+import UserMenu from "../UserMenu";
+import { Container } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingTop: 20,
     paddingBottom: 20,
-    backgroundColor: '#d8d8d8',
-    width: '500px',
+    backgroundColor: "#d8d8d8",
+    width: "500px",
   },
   back: {
-    backgroundColor: '#d8d8d8',
-    border: '1px solid black',
-    boxShadow: '0 2px 5px 2px rgb(0 0 0 / 90%)',
+    backgroundColor: "#d8d8d8",
+    border: "1px solid black",
+    boxShadow: "0 2px 5px 2px rgb(0 0 0 / 90%)",
   },
 });
 
-const MyAppBar = ({ isAuthenticated }) => {
+const MyAppBar = () => {
   const classes = useStyles();
+  const isLoggedIn = useSelector(authSelectors.getIsAuthenticated);
 
   return (
     <AppBar className={classes.back}>
       <Container className={classes.root}>
         <Navigation />
-        {isAuthenticated ? <UserMenu /> : <AuthNav />}
+        {isLoggedIn ? <UserMenu /> : <AuthNav />}
       </Container>
     </AppBar>
   );
 };
 
-const mapStateToProps = state => ({
-  isAuthenticated: authSelectors.getIsAuthenticated(state),
-});
-
-export default connect(mapStateToProps)(MyAppBar);
+export default MyAppBar;
