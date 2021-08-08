acc7edc24012eec1acc9ceb916865c01774d53da
unknown <nightsta1kerhf@gmail.com>
2021-08-08 17:16:43 +0300

    v.9.2.1

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,102 +1,93 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { TextField, Button } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save';
 import { addContact } from '../../redux/contacts/contacts-operations';
 import { getContacts } from '../../redux/contacts/contacts-selectors';
-import styles from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
 
-  handleChange = event => {
+  const handleChange = useCallback(event => {
     const { name, value } = event.target;
 
-    this.setState({
-      [name]: value,
-    });
-  };
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
 
-  handleSubmit = event => {
-    event.preventDefault();
-    const { name } = this.state;
-    const names = this.props.contacts.map(contact => contact.name);
+      case 'number':
+        setNumber(value);
+        break;
 
-    names.includes(name)
-      ? alert(`${name} is already in contacts`)
-      : this.props.onSubmit({ ...this.state });
+      default:
+        console.warn(`Тип поля ${name} не обрабатывается`);
+    }
+  }, []);
 
-    this.resetForm();
-  };
+  const handleSubmit = useCallback(
+    event => {
+      event.preventDefault();
+      const names = contacts.map(contact => contact.name);
+
+      names.includes(name)
+        ? alert(`${name} is already in contacts`)
+        : dispatch(addContact({ name, number }));
+
+      resetForm();
+    },
+    [dispatch, contacts, name, number],
+  );
 
   resetForm = () => {
     this.setState({ name: '', number: '' });
   };
 
-  render() {
-    const { name, number } = this.state;
-    const { handleSubmit, handleChange } = this;
+  const resetForm = () => {
+    setName('');
+    setNumber('');
+  };
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Name"
-          variant="outlined"
-          type="text"
-          name="name"
-          required
-          value={name}
-          onChange={handleChange}
-          margin="dense"
-          required
-        />
-        <TextField
-          label="Number"
-          variant="outlined"
-          type="tel"
-          name="number"
-          required
-          value={number}
-          onChange={handleChange}
-          margin="dense"
-          placeholder="38-0XX-XXX-XX-XX"
-          pattern="[0-9]{2}-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}"
-          required
-        />
-        {/* <Button
-          className={styles.button}
-          variant="contained"
-          color="primary"
-          type="submit"
-          size="large"
-        >
-          Add Contact
-        </Button> */}
-        <Button
-          variant="contained"
-          color="default"
-          size="large"
-          startIcon={<SaveIcon />}
-          type="submit"
-        >
-          Save Contact
-        </Button>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Name"
+        variant="outlined"
+        type="text"
+        name="name"
+        required
+        value={name}
+        onChange={handleChange}
+        margin="dense"
+        required
+      />
+      <TextField
+        label="Number"
+        variant="outlined"
+        type="tel"
+        name="number"
+        required
+        value={number}
+        onChange={handleChange}
+        margin="dense"
+        placeholder="38-0XX-XXX-XX-XX"
+        pattern="[0-9]{2}-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}"
+        required
+      />
+      <Button
+        variant="contained"
+        color="default"
+        size="large"
+        startIcon={<SaveIcon />}
+        type="submit"
+      >
+        Save Contact
+      </Button>
+    </form>
+  );
+};
 
-const mapStateToProps = state => ({
-  contacts: getContacts(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-  onSubmit: contact => {
-    return dispatch(addContact(contact));
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
+export default ContactForm;
